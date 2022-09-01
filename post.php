15c2eb28c06a48006f61a18e17dc261e7a5d1c5e
Tangkwa504 <Tangkwa25191@gmail.com>
2022-09-01 14:43:13 +0700

    HW01092022

---
+++ post.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Webboard Meaw</h1>
+    <hr>
+    <div align="center">
+    <?php echo "ต้องการดูกระทู้หมายเลข ".$_GET["id"];?><br>
+    <br>
+    <table style="border: 2px solid black;width:40%" align="center">
+    <tr><td colspan="2" style="background-color: #6CD2FE">แสดงความคิดเห็น</td></tr>
+    <tr><td><textarea type="text" name="login" size="40"></textarea></td></tr>
+        <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"a></td></tr>
+    </table>
+    <br>
+    <div align="center">
+        <a href="index.html">กลับไปหน้าหลัก</a>    
+    </div>
+    </div>
+</body>
+</html>
